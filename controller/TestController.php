3f27be3d45a4e44b052af7eee7cf01243091c653
Yusuf <yusufst000@gmail.com>
2015-04-01 03:37:34 +0600

    Framwork on progress

---
+++ controller/TestController.php
@@ -0,0 +1,7 @@
+<?php
+
+Class TestController extends Controller {
+    function checkView() {
+        $this->render();
+    }
+}
--- framwork/Controller.php
+++ framwork/Controller.php
@@ -2,4 +2,23 @@
 class Controller {
     public $name;
     public $actionName;
+    public $rootPath;
+
+    function __construct($name, $actionName) {
+        $this->rootPath = ROOT . DS . ".." . DS;
+        $this->name = $name;
+        $this->actionName = $actionName;
+    }
+
+    function render($modelView = array()) {
+        if(count($modelView) > 0 ) {
+
+        } else {
+            include($this->rootPath . DS . "view" . DS . $this->name . DS . $this->actionName . ".php");
+        }
+    }
+
+    function renderContent($modelView = array()) {
+
+    }
 };
