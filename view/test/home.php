fdaf8facf6667d6b55ec856b3b0541b87cf0c0b9
sakib <sakibmuntaheen3@gmail.com>
2015-04-20 23:34:19 +0600

    sakib's home

--- view/test/home.php
+++ view/test/home.php
@@ -1,3 +1,19 @@
-<div>
-    sakib's home
+<!DOCTYPE html>
+<html>
+
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Home Page</title>
+    <link href="/ReviewSite/css/homestyle.css" media="all" type="text/css" rel="stylesheet">
+</head>
+
+<body>
+<div class="pageLayout">
+    <div class="topRest"> Top Restaurent</div>
+    <div class="about">About</div>
+    <div class="revTab">Review Tab</div>
+    <div class="revCmnt">Review Comment</div>
 </div>
+
+</body>
+</html>
