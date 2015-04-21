979abaf9fdfdd8118e9518c70ec2da13f0962ed1
sakib <sakibmuntaheen3@gmail.com>
2015-04-21 21:11:13 +0600

    Top reviewers+search button

--- view/test/home.php
+++ view/test/home.php
@@ -10,7 +10,14 @@
 <body>
 <div class="pageLayout">
     <div class="topRest"> Top Restaurent</div>
-    <div class="about">About</div>
+    <div class="topReviewers">
+        <h2 class="title ">
+            <span class="title_left"></span>
+            <span class="title_right"></span>
+            Top Reviewers in Bangladesh
+            <a class="more" href="/">More</a>
+        </h2>
+    </div>
     <div class="revTab">Review Tab</div>
     <div class="revCmnt">Review Comment</div>
 </div>
