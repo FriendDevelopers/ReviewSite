7a71ae100abadc15721ef183e2fdaffe1ba50ed9
sakib <sakibmuntaheen3@gmail.com>
2015-04-27 15:41:31 +0600

    Write your review

--- view/test/home.php
+++ view/test/home.php
@@ -9,7 +9,72 @@
 
 <body>
 <div class="pageLayout">
-    <div class="topRest"> Top Restaurant </div>
+    <div class="myReview clearfix">
+        <h2>Write Your Review</h2>
+        <div class="propic">
+
+        </div>
+        <div class="myReview-content">
+            <div class="main-container review-widget review-widget--decorated">
+                <form method="POST" action="#">
+                    <div class="review-stars clearfix">
+                        <fieldset class="star-rating-widget inline-block" data-component-bound="true">
+                            <legend class="offscreen">Rating</legend>
+                            <ul class="stars-0">
+                                <li>
+                                    <input type="radio" value="1" name="rating" id="rating-1">
+                                    <label for="rating-1"></label>
+                                </li>
+                                <li>
+                                    <input type="radio" value="2" name="rating" id="rating-2">
+                                    <label for="rating-2"></label>
+                                </li>
+                                <li>
+                                    <input type="radio" value="3" name="rating" id="rating-3">
+                                    <label for="rating-3"></label>
+                                </li>
+                                <li>
+                                    <input type="radio" value="4" name="rating" id="rating-4">
+                                    <label for="rating-4"></label>
+                                </li>
+                                <li>
+                                    <input type="radio" value="5" name="rating" id="rating-5">
+                                    <label for="rating-5"></label>
+                                </li>
+                            </ul>
+                            <p class="description"></p>
+                        </fieldset>
+
+
+                        <div style="display:none" class="auto-save" data-component-bound="true">
+                            <small><span class="js-save-link pseudoLink">Save Now</span></small>
+                            <small data-component-bound="true" class="saving-msg" style="display: none;"></small>
+                        </div>
+                    </div>
+
+                    <input type="hidden" value="ynra-homepage" name="review_origin">
+
+                    <div class="review-textbox">
+                        <textarea autocomplete="off" class="review-textarea expanded placeholder" data-component-bound="true" style="height: 182px;">Start your review...</textarea>
+                        <div class="hidden-text-measurer" data-component-bound="true" style=""><br><br><br></div>
+                    </div>
+
+                    <div class="post-review-actions clearfix yform">
+                        <div class="pull-left">
+
+                        </div>
+                        <div class="action-buttons" data-component-bound="true" style="display: block;">
+                            <a class="cancel" href="javascript:;">Cancel</a>
+                            <button class="ybtn ybtn-primary ybtn-small ytype" value="submit" type="submit"><span>Post Review</span></button>
+                        </div>
+                        <span data-component-bound="true" class="saving-msg" style="display: none;"></span></div>
+                </form>
+
+            </div>
+
+        </div>
+    </div>
+
     <div class="topReviewers">
         <h2 class="tr_title ">
             <span class="title_left"></span>
